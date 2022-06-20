3cd968c59f310afdadfe1e7ce8746bfc7d493c71
Iann-rst <iannrst11@gmail.com>
2022-06-20 09:06:27 -0300

    Componente Button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,22 @@
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+interface ButtonProps extends RectButtonProps {
+  color?: string;
+}
+
+export const Container = styled(RectButton) <ButtonProps>`
+  width: 100%;
+  padding: 19px;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ color, theme }) => color ? color : theme.colors.main};
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`;
