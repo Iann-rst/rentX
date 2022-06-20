2ded564220aec7ade46a67edf3beb0b9bac3e68b
Iann-rst <iannrst11@gmail.com>
2022-06-20 19:33:20 -0300

    Rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+/*A documentação do React Native Gesture Handler, 
+a versão atual e as próximas necessitam que em volta de nossa aplicação utilize o 
+provider GestureHandlerRootView para os botões do tipo RectButton possam funcionar
+*/
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import { StackRoutes } from './stack.routes';
 
 export function Routes() {
   return (
-    <NavigationContainer>
-      <StackRoutes />
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <StackRoutes />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
