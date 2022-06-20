5667a3f44590d7019f8c368475bc35ec1d1c9279
Iann-rst <iannrst11@gmail.com>
2022-06-20 16:40:11 -0300

    Tela de agendamento concluído

---
+++ src/screens/SchedulingComplete/styles.ts
@@ -0,0 +1,42 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.header};
+
+  padding-top: 96px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+
+  padding-bottom: 80px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  font-size: ${RFValue(30)}px;
+  color: ${({ theme }) => theme.colors.shape};
+
+  margin-top: 40px;
+`;
+
+export const Message = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text_details};
+
+  line-height: ${RFValue(25)}px;
+  text-align: center;
+
+  margin-top: 16px;
+`;
+
+export const Footer = styled.View`
+  width:100%;
+  align-items: center;
+  margin: 80px 0;
+`;
