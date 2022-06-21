8ef3025cf42bc3101a7c5bf32f2897f8bfd748c8
Iann-rst <iannrst11@gmail.com>
2022-06-21 19:45:28 -0300

    feat: atualiza as datas de aluguel do carro na API

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -3,6 +3,7 @@ import { Feather } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useTheme } from 'styled-components';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { Alert } from 'react-native';
 
 
 import { BackButton } from '../../components/BackButton';
@@ -12,6 +13,10 @@ import { Button } from '../../components/Button';
 
 import { CarDTO } from '../../dtos/CarDTO';
 import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
+
+import { getPlatformDate } from '../../utils/getPlatformDate';
+import { format } from 'date-fns';
+import { api } from '../../services/api';
 
 import {
   Container,
@@ -38,8 +43,7 @@ import {
   RentalPriceTotal,
   Footer
 } from './styles';
-import { getPlatformDate } from '../../utils/getPlatformDate';
-import { format } from 'date-fns';
+
 
 
 interface Params {
@@ -62,8 +66,18 @@ export function SchedulingDetails() {
 
   const rentTotal = Number(dates.length * car.rent.price);
 
-  function handleConfirm() {
-    navigation.navigate('SchedulingComplete');
+  async function handleConfirm() {
+    const schedulesByCar = await api.get(`/schedules_bycars/${car.id}`);
+
+    const unavailable_dates = [
+      ...schedulesByCar.data.unavailable_dates,
+      ...dates,
+    ];
+
+    api.put(`/schedules_bycars/${car.id}`, {
+      id: car.id,
+      unavailable_dates
+    }).then(() => navigation.navigate('SchedulingComplete')).catch(() => Alert.alert('Não foi possível confirmar o agendamento.'))
   }
 
   function handleBackButton() {
