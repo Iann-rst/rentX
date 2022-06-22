66baa204f7cb1bf1d994789820bc6fdff766596d
Iann-rst <iannrst11@gmail.com>
2022-06-22 16:33:49 -0300

    Screen MyCars

---
+++ src/screens/MyCars/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { Text } from 'react-native';
+import { CarDTO } from '../../dtos/CarDTO';
+import { api } from '../../services/api';
+import { Container } from './styles';
+
+export function MyCars() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get('/schedules_byuser?user_id=1');
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, []);
+
+  return (
+    <Container>
+      <Text>MyCARS</Text>
+    </Container>
+  );
+}
