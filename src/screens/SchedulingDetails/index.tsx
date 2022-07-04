a5806f3814a967ad48bf4e286a620b7a04e6e2f8
Iann-rst <iannrst11@gmail.com>
2022-07-04 15:54:57 -0300

    feat: navegação para a tela Confirmation

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -88,7 +88,11 @@ export function SchedulingDetails() {
     api.put(`/schedules_bycars/${car.id}`, {
       id: car.id,
       unavailable_dates
-    }).then(() => navigation.navigate('SchedulingComplete'))
+    }).then(() => navigation.navigate('Confirmation', {
+      nextScreenRoute: 'Home',
+      title: 'Carro alugado!',
+      message: `Agora você só precisa ir \naté a concessionária da RENTX\npegar o seu automóvel.`
+    }))
       .catch(() => {
         setLoading(false);
         Alert.alert('Não foi possível confirmar o agendamento.')
