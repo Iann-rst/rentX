d7adda5bb0851ceb4b161685a844e8f3ed79f0e8
Iann-rst <iannrst11@gmail.com>
2022-07-04 19:25:46 -0300

    rotas para navegações do tipo Stack

---
+++ src/routes/app.stack.routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//Telas para navegações stack
+import { Home } from '../screens/Home';
+import { CarDetails } from '../screens/CarDetails';
+import { Scheduling } from '../screens/Scheduling';
+import { SchedulingDetails } from '../screens/SchedulingDetails';
+import { Confirmation } from '../screens/Confirmation';
+import { MyCars } from '../screens/MyCars';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppStackRoutes() {
+  return (
+    <Navigator screenOptions={{
+      headerShown: false
+    }}
+      initialRouteName="Home"
+    >
+      <Screen name="Home" component={Home} />
+      <Screen name="CarDetails" component={CarDetails} />
+      <Screen name="Scheduling" component={Scheduling} />
+      <Screen name="SchedulingDetails" component={SchedulingDetails} />
+      <Screen name="Confirmation" component={Confirmation} />
+      <Screen name="MyCars" component={MyCars} />
+    </Navigator>
+  )
+}
