ad96680f077b535ce6a0d898b89b14a6093c13f6
Iann-rst <iannrst11@gmail.com>
2022-07-04 14:50:48 -0300

    feat: Validação do Cadastro

--- src/screens/SignUp/FirstStep/index.tsx
+++ src/screens/SignUp/FirstStep/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert
+} from 'react-native';
 
+import * as Yup from 'yup';
 
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
 
 import {
   Container,
@@ -12,38 +21,93 @@ import {
   Title,
   SubTitle,
   Form,
-  FormTitle
+  FormTitle,
+  Footer
 } from './styles';
 
 export function FirstStep() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [cnh, setCnh] = useState('');
+
   const navigation = useNavigation<any>();
 
   function handleBack() {
     navigation.goBack();
   }
+
+  async function handleNextStep() {
+    try {
+      const schema = Yup.object().shape({
+        cnh: Yup.string().required('CNH é obrigatória'),
+        email: Yup.string().email('E-mail inválido').required('E-mail é obrigatório'),
+        name: Yup.string().required('Nome é obrigatório')
+      });
+
+      const data = { name, email, cnh };
+      await schema.validate(data);
+      navigation.navigate('SecondStep', { user: data });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        return Alert.alert('Opa', error.message);
+      }
+    }
+  }
+
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
 
-      <Title>
-        Crie sua {'\n'}
-        Conta
-      </Title>
+          <Title>
+            Crie sua {'\n'}
+            Conta
+          </Title>
 
-      <SubTitle>
-        Faça seu cadastro de{'\n'}
-        forma rápida e fácil
-      </SubTitle>
+          <SubTitle>
+            Faça seu cadastro de{'\n'}
+            forma rápida e fácil
+          </SubTitle>
 
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input
+              iconName='user'
+              placeholder="Nome"
+              onChangeText={setName}
+              value={name}
+            />
+            <Input
+              iconName='mail'
+              placeholder="E-mail"
+              keyboardType="email-address"
+              onChangeText={setEmail}
+              value={email}
+            />
+            <Input
+              iconName='credit-card'
+              placeholder="CNH"
+              keyboardType="numeric"
+              onChangeText={setCnh}
+              value={cnh}
+            />
+          </Form>
+
+          <Footer>
+            <Button
+              title='Próximo'
+              onPress={handleNextStep}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/SignUp/FirstStep/styles.ts
+++ src/screens/SignUp/FirstStep/styles.ts
@@ -3,7 +3,6 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  flex: 1;
   padding: 0 24px;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
@@ -49,4 +48,7 @@ export const FormTitle = styled.Text`
   color: ${({ theme }) => theme.colors.title};
   font-size: ${RFValue(20)}px;
   font-family: ${({ theme }) => theme.fonts.secondary_600};
+  margin-bottom: 24px;
 `;
+
+export const Footer = styled.View``;
