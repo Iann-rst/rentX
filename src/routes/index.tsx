4d06cd9616e31d4d567c66397c4dcc1ac5b02d6b
Iann-rst <iannrst11@gmail.com>
2022-07-04 19:26:56 -0300

    Rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { useAuth } from '../hooks/auth';
 
 /*A documentação do React Native Gesture Handler, 
 a versão atual e as próximas necessitam que em volta de nossa aplicação utilize o 
@@ -7,13 +8,16 @@ provider GestureHandlerRootView para os botões do tipo RectButton possam funcio
 */
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import { StackRoutes } from './stack.routes';
+import { AppTabRoutes } from './app.tab.routes';
+import { AuthRoutes } from './auth.routes';
+
 
 export function Routes() {
+  const { user } = useAuth();
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
-        <StackRoutes />
+        {user ? <AppTabRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </GestureHandlerRootView>
   );
