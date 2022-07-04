60c9f11ae84034b7cd2dcfa8c9f13fb090050fe4
Iann-rst <iannrst11@gmail.com>
2022-07-04 12:50:35 -0300

    Componente Bullet

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+interface BulletProps {
+  active?: boolean;
+}
+
+export function Bullet({ active = false }: BulletProps) {
+  return (
+    <Container active={active} />
+  );
+}
