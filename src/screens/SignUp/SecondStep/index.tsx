4eac31ba83bf303f0892cbff356e0a6f2a948cbb
Iann-rst <iannrst11@gmail.com>
2022-07-04 14:23:52 -0300

    Screen SignUp: SecondStep

---
+++ src/screens/SignUp/SecondStep/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native';
+
+
+import { BackButton } from '../../../components/BackButton';
+import { Bullet } from '../../../components/Bullet';
+import { Button } from '../../../components/Button';
+import { PasswordInput } from '../../../components/PasswordInput';
+
+import {
+  Container,
+  Header,
+  Steps,
+  Title,
+  SubTitle,
+  Form,
+  FormTitle,
+  Footer
+} from './styles';
+
+export function SecondStep() {
+  const theme = useTheme();
+
+  const navigation = useNavigation<any>();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet />
+              <Bullet active />
+            </Steps>
+          </Header>
+
+          <Title>
+            Crie sua {'\n'}
+            Conta
+          </Title>
+
+          <SubTitle>
+            Faça seu cadastro de{'\n'}
+            forma rápida e fácil
+          </SubTitle>
+
+          <Form>
+            <FormTitle>02. Senha</FormTitle>
+            <PasswordInput iconName="lock" placeholder="Senha" />
+            <PasswordInput iconName="lock" placeholder="Repetir senha" />
+          </Form>
+
+          <Footer>
+            <Button
+              title='Cadastrar'
+              onPress={() => { }}
+              color={theme.colors.success}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
