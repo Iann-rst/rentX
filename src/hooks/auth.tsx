9db643c44fc9cb9a0178268e96ee2019cbc15e26
Iann-rst <iannrst11@gmail.com>
2022-07-06 15:37:26 -0300

    feat: Atualizando Profile

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -33,6 +33,7 @@ interface AuthContextData {
   user: User;
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => Promise<void>;
+  updatedUser: (user: User) => Promise<void>;
 }
 
 interface AuthProviderProps {
@@ -90,6 +91,24 @@ function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  //Atualizar os dados do usuário
+  async function updatedUser(user: User) {
+    try {
+      const userCollection = database.get<ModelUser>('users');
+      await database.write(async () => {
+        const userSelected = await userCollection.find(user.id);
+        await userSelected.update((userData) => {
+          userData.name = user.name,
+            userData.driver_license = user.driver_license,
+            userData.avatar = user.avatar
+        });
+      });
+      setData(user);
+
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
   useEffect(() => {
     async function loadUserData() {
       const userCollection = database.get<ModelUser>('users');
@@ -109,7 +128,8 @@ function AuthProvider({ children }: AuthProviderProps) {
     <AuthContext.Provider value={{
       user: data,
       signIn,
-      signOut
+      signOut,
+      updatedUser
     }}>
       {children}
     </AuthContext.Provider>
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import * as ImagePicker from 'expo-image-picker';
+
+import * as Yup from 'yup';
 
 import { useAuth } from '../../hooks/auth';
 
@@ -13,6 +15,7 @@ import { Feather } from '@expo/vector-icons';
 import { BackButton } from '../../components/BackButton';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+import { Button } from '../../components/Button';
 
 
 import {
@@ -32,8 +35,9 @@ import {
 } from './styles';
 
 export function Profile() {
+  const { user, signOut, updatedUser } = useAuth();
+
   const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
-  const { user, signOut } = useAuth();
   const [avatar, setAvatar] = useState(user.avatar);
   const [name, setName] = useState(user.name);
   const [driverLicense, setDriverLicense] = useState(user.driver_license);
@@ -46,6 +50,7 @@ export function Profile() {
   }
 
 
+  //Função de signOut
   function handleSignOut() {
     signOut();
   }
@@ -70,6 +75,37 @@ export function Profile() {
 
   function handleOptionChange(optionSelected: 'dataEdit' | 'passwordEdit') {
     setOption(optionSelected);
+  }
+
+  async function handleProfileUpdate() {
+    try {
+      const schema = Yup.object().shape({
+        driverLicense: Yup.string().required('CNH é obrigatória'),
+        name: Yup.string().required('Nome é obrigatório')
+      });
+
+      const data = { name, driverLicense };
+      await schema.validate(data);
+
+      await updatedUser({
+        id: user.id,
+        user_id: user.user_id,
+        email: user.email,
+        name,
+        driver_license: driverLicense,
+        avatar,
+        token: user.token
+      });
+
+      Alert.alert('Perfil atualizado!');
+
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert('Opa', error.message);
+      } else {
+        Alert.alert('Não foi possível atualizar o perfil');
+      }
+    }
   }
 
   return (
@@ -159,6 +195,7 @@ export function Profile() {
                 </Section>
 
             }
+            <Button title="Salvar alterações" onPress={handleProfileUpdate} />
           </Content>
 
         </Container>
