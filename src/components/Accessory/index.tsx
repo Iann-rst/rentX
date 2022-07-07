33e13c0afafec1d319edcac97ea44d48fc06b54b
Iann-rst <iannrst11@gmail.com>
2022-07-07 10:54:50 -0300

    fix: cor dos acessorios (svg)

--- src/components/Accessory/index.tsx
+++ src/components/Accessory/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SvgProps } from 'react-native-svg';
+import { useTheme } from 'styled-components';
 
 import {
   Container,
@@ -16,9 +17,10 @@ export function Accessory({
   icon: Icon
 }: Props
 ) {
+  const theme = useTheme();
   return (
     <Container>
-      <Icon width={32} height={32} />
+      <Icon width={32} height={32} fill={theme.colors.header} />
       <Name>{name}</Name>
     </Container>
   );
